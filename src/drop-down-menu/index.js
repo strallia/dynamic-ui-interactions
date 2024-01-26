e203110c6e69689585b64334eddd52385ad8ff6c
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-26 15:17:52 -0800

    Create function to reset nav background colors

--- src/drop-down-menu/index.js
+++ src/drop-down-menu/index.js
@@ -2,21 +2,23 @@ import './styles.css';
 
 const nav = document.querySelector('nav');
 
-const colorTitleOnTitleHover = (titleNode) => {
+const resetNavBackground = () => {
   const titles = nav.querySelectorAll('nav > ul li');
   titles.forEach((title) => {
     title.classList.remove('color-title');
   });
 
-  titleNode.classList.add('color-title');
-};
-
-const colorDropDownOnTitleHover = (titleNode) => {
   const dropDowns = nav.querySelectorAll('.drop-down');
   dropDowns.forEach((dropDown) => {
     dropDown.classList.remove('color-column');
   });
+};
 
+const colorTitleOnTitleHover = (titleNode) => {
+  titleNode.classList.add('color-title');
+};
+
+const colorDropDownOnTitleHover = (titleNode) => {
   const titleValue = titleNode.getAttribute('data-title');
   const coloredDropDown = nav.querySelector(
     `.drop-down[data-title="${titleValue}"]`,
@@ -27,17 +29,13 @@ const colorDropDownOnTitleHover = (titleNode) => {
 const navTitles = nav.querySelectorAll('nav > ul li');
 navTitles.forEach((title) => {
   title.addEventListener('mouseover', (e) => {
+    resetNavBackground();
     colorTitleOnTitleHover(e.target);
     colorDropDownOnTitleHover(e.target);
   });
 });
 
 const colorTitleOnDropDownHover = (dropDownNode) => {
-  const titles = nav.querySelectorAll('nav > ul li');
-  titles.forEach((title) => {
-    title.classList.remove('color-title');
-  });
-
   const titleValue = dropDownNode.getAttribute('data-title');
   const titleNode = nav.querySelector(
     `nav > ul li[data-title="${titleValue}"]`,
@@ -46,18 +44,17 @@ const colorTitleOnDropDownHover = (dropDownNode) => {
 };
 
 const colorDropDownOnDropDownHover = (dropDownNode) => {
-  const dropDowns = nav.querySelectorAll('.drop-down');
-  dropDowns.forEach((dropDown) => {
-    dropDown.classList.remove('color-column');
-  });
-
   dropDownNode.classList.add('color-column');
 };
 
 const dropDownListItems = nav.querySelectorAll('.drop-down li');
 dropDownListItems.forEach((item) => {
   item.addEventListener('mouseover', (e) => {
+    resetNavBackground();
     colorTitleOnDropDownHover(e.target.parentNode);
     colorDropDownOnDropDownHover(e.target.parentNode);
   });
 });
+
+// TODO: make links clickable but not in the way of hover pointer events
+// TODO: remove title background colors when hover out of nav
