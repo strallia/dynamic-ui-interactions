dd882c6b134798475742aee66a9e7264e78809ef
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-26 14:24:35 -0800

    Change background color when hover on nav titles

--- src/drop-down-menu/index.js
+++ src/drop-down-menu/index.js
@@ -1,1 +1,29 @@
-import "./styles.css";
+import './styles.css';
+
+const colorTitle = function colorTitleBackground(titleNode) {
+  const titles = document.querySelectorAll('nav > ul li');
+  titles.forEach((title) => {
+    title.classList.remove('color-title');
+  });
+
+  titleNode.classList.add('color-title');
+};
+
+const colorDropDown = function colorTitleDropDown(title) {
+  const dropDowns = document.querySelectorAll('.drop-down');
+  dropDowns.forEach((dropDown) => {
+    dropDown.classList.remove('color-column');
+  });
+
+  const titleValue = title.getAttribute('data-value');
+  const coloredDropDown = document.querySelector(`.drop-down.${titleValue}`);
+  coloredDropDown.classList.add('color-column');
+};
+
+const navTitles = document.querySelectorAll('nav > ul li');
+navTitles.forEach((title) => {
+  title.addEventListener('mouseover', (e) => {
+    colorTitle(e.target);
+    colorDropDown(e.target);
+  });
+});
