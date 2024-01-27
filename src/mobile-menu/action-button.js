4c287cd8e283b82dec3fab4c026f9af1627eed9c
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-27 11:09:02 -0800

    Toggle between add-button and menu views on click

--- src/mobile-menu/action-button.js
+++ src/mobile-menu/action-button.js
@@ -2,3 +2,14 @@ import './styles.css';
 import './menu';
 
 const container = document.querySelector('.container');
+
+const addButtonView = container.querySelector('.add-button-view');
+const menuView = container.querySelector('.menu-view');
+const toggleViews = () => {
+  addButtonView.classList.toggle('hidden');
+  menuView.classList.toggle('hidden');
+};
+addButtonView.addEventListener('click', toggleViews);
+
+const menuBtns = menuView.querySelector('button');
+menuBtns.addEventListener('click', toggleViews);
