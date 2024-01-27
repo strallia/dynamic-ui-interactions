6472fdbdcf984a291e859a496751a7cd8e1ffb05
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-27 11:48:28 -0800

    Add menu items dynamically

--- src/mobile-menu/index.js
+++ src/mobile-menu/index.js
@@ -1,4 +1,8 @@
 import './styles.css';
+import Camera from './icons/camera.png';
+import PaperClip from './icons/paper-clip.png';
+import Chat from './icons/chat.png';
+import Clock from './icons/clock.png';
 
 const container = document.querySelector('.container');
 
@@ -10,5 +14,50 @@ const toggleViews = () => {
 };
 addButtonView.addEventListener('click', toggleViews);
 
-const menuBtns = menuView.querySelector('button');
-menuBtns.addEventListener('click', toggleViews);
+const menuItems = [
+  {
+    title: 'Camera',
+    img: Camera,
+  },
+  {
+    title: 'Attachment',
+    img: PaperClip,
+  },
+  {
+    title: 'Work Chat',
+    img: Chat,
+  },
+  {
+    title: 'Reminder',
+    img: Clock,
+  },
+];
+
+const appendChildren = (parentNode, childrenArr) => {
+  childrenArr.forEach((child) => {
+    parentNode.appendChild(child);
+  });
+};
+
+const createTabNode = (item) => {
+  const tabDiv = document.createElement('div');
+  const titlePara = document.createElement('p');
+  const btn = document.createElement('button');
+  const icon = document.createElement('img');
+
+  tabDiv.classList.add('tab');
+  btn.classList.add('small');
+  titlePara.setAttribute('data-id', item.title.toLowerCase());
+  btn.setAttribute('data-id', item.title.toLowerCase());
+  icon.setAttribute('src', item.img);
+
+  titlePara.textContent = item.title;
+
+  btn.addEventListener('click', toggleViews);
+
+  btn.appendChild(icon);
+  appendChildren(tabDiv, [titlePara, btn]);
+  menuView.appendChild(tabDiv);
+};
+
+menuItems.forEach(createTabNode);
